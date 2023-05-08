a2de05112b3db909c377a8ccead23db2aacb390a
김근호 <kimkgh8@gmail.com>
2023-05-08 22:51:47 +0900

    ✨ Feat : FAQ 페이지

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,8 @@
 //using selectors inside the element
 // traversing the dom
+
+document.querySelectorAll(".question-btn").forEach(btn => {
+	btn.addEventListener("click", function() {
+		btn.parentNode.parentNode.classList.toggle("show-text");
+	});
+});
